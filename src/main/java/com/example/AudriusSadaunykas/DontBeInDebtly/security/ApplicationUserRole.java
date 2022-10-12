208ea7b7e82a00f8077f6df8be0463b8ea33a8d3
Audrius Sadaunykas <audriussad@gmail.com>
2022-10-12 15:53:59 +0300

    test

--- src/main/java/com/example/AudriusSadaunykas/DontBeInDebtly/security/ApplicationUserRole.java
+++ src/main/java/com/example/AudriusSadaunykas/DontBeInDebtly/security/ApplicationUserRole.java
@@ -7,7 +7,7 @@ import java.util.Set;
 import static com.example.AudriusSadaunykas.DontBeInDebtly.security.ApplicationUserPermission.*;
 
 public enum ApplicationUserRole {
-    USER(Sets.newHashSet()),
+    USER(Sets.newHashSet(BUDGET_ITEM_WRITE, BUDGET_ITEM_READ,  TRANSACTION_READ, TRANSACTION_WRITE)),
     ADMIN(Sets.newHashSet(BUDGET_ITEM_WRITE, BUDGET_ITEM_READ, TRANSACTION_READ, TRANSACTION_WRITE));
 
     private final Set<ApplicationUserPermission> permissions;
